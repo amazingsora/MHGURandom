7e33397ffb280d80f8c9a1ff60a7d50ce35f9286
amazingSora <lgaz830304@gmail.com>
2026-06-05 17:52:34 +0800

    Refactoring

--- MHGURandon/Form1.cs
+++ MHGURandon/Form1.cs
@@ -1,83 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 
 namespace MHGURandon
 {
     public partial class Form1 : Form
     {
-        int p = 1;
-        
-        /*
-      catcheckbox 1有 0沒有
-      */
-        int catcheckbox = 1;
-        int index = 0;
-        string[] styleName ;
-        Weapon[] weapons;
+        private readonly Random _random = new Random();
+
+        private readonly string[] _styleNames =
+        {
+            "公會風格", "強襲風格", "空戰風格",
+            "武士道",   "英勇風格", "煉金風格"
+        };
+
+        private readonly Weapon[] _weapons;
+
+        private bool _includeCat = true;
+        private int _rollCount = 0;
+
+        private const int CatIndex = 14;
+
         public Form1()
         {
-            styleName = new string[7];
-            styleName[0]="公會風格";
-            styleName[1] = "強襲風格";
-            styleName[2] = "空戰風格";
-            styleName[3] = "武士道";
-
-            styleName[4] = "英勇風格";
-            styleName[5] = "煉金風格";
-            styleName[6] = "";
-
-            weapons = new Weapon[15];
-            weapons[0]=new Weapon(0,"大劍",Properties.Resources._000);
-            weapons[1]=new Weapon(1, "太刀", Properties.Resources._001);
-            weapons[2]=new Weapon(2, "單手劍", Properties.Resources._002);
-            weapons[3]=new Weapon(3, "雙劍", Properties.Resources._003);
-            weapons[4]=new Weapon(4, "大槌", Properties.Resources._004);
-            weapons[5]=new Weapon(5, "狩獵笛", Properties.Resources._005);
-            weapons[6] = new Weapon(6, "長槍", Properties.Resources._006);
-            weapons[7] = new Weapon(7, "銃槍", Properties.Resources._007);
-            weapons[8] = new Weapon(8, "斬擊斧", Properties.Resources._008);
-            weapons[9] = new Weapon(9, "操蟲棍", Properties.Resources._009);
-            weapons[10] = new Weapon(10, "充能斧", Properties.Resources._010);
-            weapons[11] = new Weapon(11, "輕弩", Properties.Resources._011);
-            weapons[12] = new Weapon(12, "重弩", Properties.Resources._012);
-            weapons[13] = new Weapon(13, "弓", Properties.Resources._013);
-            weapons[14] = new Weapon(14, "獵喵", Properties.Resources._014);
+            _weapons = new[]
+            {
+                new Weapon(0,  "大劍",   Properties.Resources._000),
+                new Weapon(1,  "太刀",   Properties.Resources._001),
+                new Weapon(2,  "單手劍", Properties.Resources._002),
+                new Weapon(3,  "雙劍",   Properties.Resources._003),
+                new Weapon(4,  "大槌",   Properties.Resources._004),
+                new Weapon(5,  "狩獵笛", Properties.Resources._005),
+                new Weapon(6,  "長槍",   Properties.Resources._006),
+                new Weapon(7,  "銃槍",   Properties.Resources._007),
+                new Weapon(8,  "斬擊斧", Properties.Resources._008),
+                new Weapon(9,  "操蟲棍", Properties.Resources._009),
+                new Weapon(10, "充能斧", Properties.Resources._010),
+                new Weapon(11, "輕弩",   Properties.Resources._011),
+                new Weapon(12, "重弩",   Properties.Resources._012),
+                new Weapon(13, "弓",     Properties.Resources._013),
+                new Weapon(14, "獵喵",   Properties.Resources._014),
+            };
 
             InitializeComponent();
         }
 
+        // 回傳 (武器索引, 風格索引)；獵喵時風格索引為 -1
+        private (int weaponIndex, int styleIndex) Roll()
+        {
+            int limit = _includeCat ? _weapons.Length : _weapons.Length - 1;
+            int weaponIndex = _random.Next(0, limit);
+            int styleIndex  = weaponIndex == CatIndex ? -1 : _random.Next(0, _styleNames.Length);
+            return (weaponIndex, styleIndex);
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            index++;
-            Random r = new Random();
-            int i1;
-            if (catcheckbox == 1) i1 = r.Next(0, 15);
-            else i1 = r.Next(0, 14);
+            var (wi, si) = Roll();
+            _rollCount++;
 
-            int i2 = r.Next(0,6);
-          
-            if (i1 == 14) {
-                i2 = 6;
-            }
+            weaponBox.Image      = _weapons[wi].Image;
+            weponnamelabel.Text  = _weapons[wi].Name;
+            stylenamelabel.Text  = si >= 0 ? _styleNames[si] : "";
 
-            stylenamelabel.Text = styleName[i2];
-            weaponBox.Image = weapons[i1].Image;
-            weponnamelabel.Text = weapons[i1].Name;
-            if (i1 < 14)
+            string entry = wi == CatIndex
+                ? $"第{_rollCount}次結果：獵喵"
+                : $"第{_rollCount}次結果：{_styleNames[si]} X {_weapons[wi].Name}";
+            listBox.Items.Add(entry);
+        }
+
+        private void random4_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            for (int player = 1; player <= 4; player++)
             {
-                listBox.Items.Add("第" + index + "次結果:" + styleName[i2] + "X" + weapons[i1].Name);
-            }
-            else if(i1==14){
-                listBox.Items.Add("第" + index + "次結果: 獵喵");
-
+                var (wi, si) = Roll();
+                string entry = wi == CatIndex
+                    ? $"P{player} 請用：獵喵"
+                    : $"P{player} 請用：{_styleNames[si]} X {_weapons[wi].Name}";
+                listBox.Items.Add(entry);
             }
         }
 
@@ -86,72 +86,26 @@ namespace MHGURandon
             listBox.Items.Clear();
         }
 
-       
-        private void pictureBox3_Click(object sender, EventArgs e)
+        private void pictureBox3_Click(object sender, EventArgs e) => ToggleCat();
+        private void Catlabel_Click(object sender, EventArgs e)    => ToggleCat();
+
+        private void ToggleCat()
         {
-
-
-
-
-            if (catcheckbox == 0)
+            _includeCat = !_includeCat;
+            if (_includeCat)
             {
-                catcheckbox++;
                 pictureBox3.Image = Properties.Resources._014no;
-                Catlabel.Text = "不要獵喵";
-                catmessage.Text = "";
-
+                Catlabel.Text     = "不要獵喵";
+                catmessage.Text   = "";
             }
-            else if(catcheckbox==1){
-                catcheckbox = 0;
+            else
+            {
                 pictureBox3.Image = Properties.Resources._014;
-                Catlabel.Text = "加入獵喵";
-                catmessage.Text = "已移除獵喵";
+                Catlabel.Text     = "加入獵喵";
+                catmessage.Text   = "已移除獵喵";
             }
         }
 
-        private void Catlabel_Click(object sender, EventArgs e)
-        {
-            pictureBox3_Click( sender, e);
-        }
-
-        private void panel3_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void random4_Click(object sender, EventArgs e)
-        {
-            listBox.Items.Clear();
-            for (int ii = 0; ii < 4; ii++) {
-                int ms = 30;
-                Random r = new Random();
-                int i1;
-                if (catcheckbox == 1) i1 = r.Next(0, 15);
-                else i1 = r.Next(0, 14);
-
-                int i2 = r.Next(0, 6);
-
-                if (i1 == 14)i2 = 6;
-    
-                if (i1 < 14)
-                {
-                    listBox.Items.Add("P" + p + "請用:" + styleName[i2] + "X" + weapons[i1].Name);
-                }
-                else if (i1 == 14)
-                {
-                    listBox.Items.Add("P" + p + "請用: 獵喵");
-
-                }
-                p++;
-                if (p == 5)
-                {
-                    p = 1;
-                }
-                Thread.Sleep(ms += ms);
-
-
-            }
-
-        }
+        private void panel3_Paint(object sender, System.Windows.Forms.PaintEventArgs e) { }
     }
 }
--- MHGURandon/Weapon.cs
+++ MHGURandon/Weapon.cs
@@ -1,28 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MHGURandon
 {
     class Weapon
     {
-        private int no;
-        private string name;
-        private Image image;
+        public int    No    { get; }
+        public string Name  { get; }
+        public Image  Image { get; }
 
         public Weapon(int no, string name, Image image)
         {
-            this.No = no;
-            this.Name = name;
-            this.Image = image;
+            No    = no;
+            Name  = name;
+            Image = image;
         }
-
-        public int No { get => no; set => no = value; }
-        public string Name { get => name; set => name = value; }
-        public Image Image { get => image; set => image = value; }
     }
-
 }
